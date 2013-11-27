173af2f680a4d98211b614c098059849fc753a26
KheOps <kheops@ceops.eu>
2013-11-27 21:34:21 +0000

    Add build.sh, a script that takes a document name and uses a file by the same name describing the document in order to produce a PDF.
    This is quick and dirty.
    
    Signed-off-by: KheOps <kheops@ceops.eu>

---
+++ doc-build/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# We want a docname
+DOCNAME=$1
+
+# Default language is English ("en")
+LANGUAGE=${2:-en}
+
+CONTENTDIR=../content-parts/$LANGUAGE
+DOCDIR=.
+DOCPATH="$DOCDIR/$DOCNAME"
+
+if [ ! -r "$DOCPATH" ]; then
+  echo "Error: cannot read $DOCPATH, did you specify a correct document?"
+  exit 1
+fi
+
+if ! which pandoc >/dev/null 2>&1; then
+  echo "Error: pandoc is required to produce the document"
+  exit 1
+fi
+
+. $DOCPATH | pandoc -o "$DOCNAME.pdf" -
